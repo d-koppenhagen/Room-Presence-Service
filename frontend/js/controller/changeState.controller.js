6406557f72297f00bf3e55a648ce1c8939f2c676
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-02-02 17:00:40 +0100

    switching to angular js -> completed

--- frontend/js/controller/changeState.controller.js
+++ frontend/js/controller/changeState.controller.js
@@ -2,71 +2,67 @@
     "use strict";
 	angular
 		.module('presenceService')
-        .controller('changeStateCtrl', ['$scope','$location',
+        .controller('changeStateCtrl', ['$scope','$http','$location',
             function($scope, $http, $location) {
-                console.log('open changeStateCtrl...');
-        }]);
-}());
+            console.log('open changeStateCtrl...');
 
+            var strBtnGreen = "btn btn-lg btn-block big-btn btn-success";
+            var strBtnRed = "btn btn-lg btn-block big-btn btn-danger";
 
-getStates();
-setInterval("getStates();",5000);
-var currentStates;
+            getData();
+            function getData() {
+                /* getting current data */
+                $http.get(rootURL+"/api/state/get")
+                    .success(response)
+                    .error(function(data, status, headers, config) {
+                    console.log("Error by getting data", data, status, headers, config);
+                });
+                function response(data){
+                    console.log("received new data: ", data);
+                    angular.forEach(data, function(value, key) {
+                        switch(value.state) {
+                            case "on": data[key]["btnClass"] = strBtnGreen;
+                                break;
+                            case "off": data[key]["btnClass"] = strBtnRed;
+                                break;
+                            default: // $("#btn_"+element.user).addClass("btn-default");
+                                ;
+                        }
+                    });
+                    $scope.persons = data;
+                    console.log("added classes:",data);
+                }
+            }
+            /* set new state */
+            $scope.toggleState = function (person, currentState){
+                var newState = "";
+                switch(currentState) {
+                    case "on": newState = "off";
+                        break;
+                    case "off": newState = "on";
+                        break;
+                    default: // $("#btn_"+element.user).addClass("btn-default");
+                        ;
+                }
 
+                var obj = {
+                    "state" : newState
+                };
+                $http.put(rootURL+"/api/state/set/"+person, JSON.stringify(obj) )
+                    .success(success)
+                    .error(function(data, status, headers, config) {
+                    console.log("Error by getting data", data, status, headers, config);
+                });
 
-$(".big-btn").click(function () {
-    changeStateUser = $(this)[0].id.split("_")[1]; // split id to name
-    console.log("changing state from: " + changeStateUser);
-
-    var oldState = currentStates[changeStateUser].state;
-    console.log("Old state: ",oldState);
-    switch(oldState) {
-        case "on": setState(changeStateUser, "off");
-            break;
-        case "off": setState(changeStateUser, "on");
-            break;
-        default: console.error("not a valid state! - your state is: ", oldState);
-    }
-
-    //setState("dogi", "on");
-    function setState(username, newState) {
-        var obj = {
-            "state" : newState
-        };
-        $.ajax({
-          url: rootURL+"/api/state/set/"+username,
-          type: "PUT",
-          data: JSON.stringify(obj),
-          contentType: "application/json",
-          success: success,
-        });
-        function success(data){
-            console.log("changed state successfully! ", data);
-            getStates();
-        }
-    }
-});
-
-
-function getStates(){
-    $.ajax({
-      url: rootURL+"/api/state/get",
-      success: response,
-    });
-
-    function response(data){
-        console.log("received new data: ", data);
-        currentStates = data;
-        $.each(data, function (key, val){
-            // set in initial states for each button
-            switch(val.state) {
-                case "on": $("#btn_"+val.username).attr("class","btn btn-lg btn-block big-btn btn-success");
-                    break;
-                case "off": $("#btn_"+val.username).attr("class","btn btn-lg btn-block big-btn btn-danger");
-                    break;
-                default: // $("#btn_"+element.user).addClass("btn-default");
-                    ;
+                function success(data){
+                    console.log("changed state successfully! ", data);
+                    getData();
+                }
+            }
+        }])
+        .filter('capitalize', function() {
+            return function(input, all) {
+                return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
             }
         });
-    }
-}
+}());
